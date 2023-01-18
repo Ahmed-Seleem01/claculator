7e7681857349e110e375736a49ac8b2247c9d4c4
Ahmed-Seleem01 <ahmedseleem.c@gmail.com>
2023-01-18 23:12:20 +0200

    Add event listener to operator keys   assign specific operation in a variable to use it after

--- js/app.js
+++ js/app.js
@@ -12,16 +12,34 @@ function divide(x, y) {
   return x / y;
 }
 
-function operate(operation, x, y){
+function operate(operation, x, y) {
   return operation(x, y);
 }
 
-let firstNumber = '';
+let firstNumber = "";
 
-const numbers = document.querySelectorAll('.num');
-for (const num of numbers){
-  num.addEventListener('click', (e)=>{
-    firstNumber+=e.target.textContent;
-  })
+const numbers = document.querySelectorAll(".num");
+for (const num of numbers) {
+  num.addEventListener("click", (e) => {
+    firstNumber += e.target.textContent;
+  });
 }
+
+let operation = "";
+const sumKey = document.querySelector(".sum");
+sumKey.addEventListener("click", () => {
+  operation = sum;
+});
+const subtractKey = document.querySelector(".sub");
+subtractKey.addEventListener("click", () => {
+  operation = subtract;
+});
+const multiplyKey = document.querySelector(".mul");
+multiplyKey.addEventListener("click", () => {
+  operation = multiply;
+});
+const divideKey = document.querySelector(".divid");
+divideKey.addEventListener("click", () => {
+  operation = divide;
+});
 
