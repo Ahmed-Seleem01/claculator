d6ff2155965186998a69e3414787d9357aa26ce0
Ahmed-Seleem01 <ahmedseleem.c@gmail.com>
2023-01-18 22:29:23 +0200

    Link external css and js files

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 // Add basic math operations
-function add(x, y){
-  return (+x) + (+y);
+function add(x, y) {
+  return +x + +y;
 }
-function subtract(x, y){
-  return x-y;
+function subtract(x, y) {
+  return x - y;
 }
-function multiply(x, y){
-  return x*y;
+function multiply(x, y) {
+  return x * y;
 }
-function divide(x, y){
-  return x/y;
+function divide(x, y) {
+  return x / y;
 }
